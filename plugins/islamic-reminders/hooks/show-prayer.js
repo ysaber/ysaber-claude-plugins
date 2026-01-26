716d35d02ab8392e3642c01923fb28261031c230
Yusuf Saber <ysaber0@gmail.com>
2026-01-26 09:42:17 +0400

    Revert "Reverse Arabic characters for better terminal display"
    This reverts commit d4d541bdc9422783a92a197aa31bbaf8fe6e2ac8.

--- plugins/islamic-reminders/hooks/show-prayer.js
+++ plugins/islamic-reminders/hooks/show-prayer.js
@@ -1,25 +1,21 @@
 #!/usr/bin/env node
 
-// Reverse string for RTL display in terminals
-const reverse = (str) => [...str].reverse().join('');
-
 const prayers = [
-  { english: "Glory be to Allah", arabic: "سبحان الله" },
-  { english: "All praise is due to Allah", arabic: "الحمد لله" },
-  { english: "Allah is the Greatest", arabic: "الله اكبر" },
-  { english: "There is no god but Allah", arabic: "لا اله الا الله" },
-  { english: "I seek forgiveness from Allah", arabic: "استغفر الله" },
-  { english: "Glory be to Allah and praise Him", arabic: "سبحان الله وبحمده" },
-  { english: "Glory be to Allah, the Magnificent", arabic: "سبحان الله العظيم" },
-  { english: "No power or strength except with Allah", arabic: "لا حول ولا قوة الا بالله" },
-  { english: "I put my trust in Allah", arabic: "توكلت على الله" },
-  { english: "What Allah wills", arabic: "ما شاء الله" }
+  "SubhanAllah - Glory be to Allah",
+  "Alhamdulillah - All praise is due to Allah",
+  "Allahu Akbar - Allah is the Greatest",
+  "La ilaha illallah - There is no god but Allah",
+  "Astaghfirullah - I seek forgiveness from Allah",
+  "SubhanAllahi wa bihamdihi - Glory be to Allah and praise Him",
+  "SubhanAllahil Azeem - Glory be to Allah, the Magnificent",
+  "La hawla wa la quwwata illa billah - No power or strength except with Allah",
+  "Tawakkaltu 'ala Allah - I put my trust in Allah",
+  "Masha'Allah - What Allah wills"
 ];
 
-const prayer = prayers[Math.floor(Math.random() * prayers.length)];
-const message = `${prayer.english} - ${reverse(prayer.arabic)}`;
+const randomPrayer = prayers[Math.floor(Math.random() * prayers.length)];
 
 console.log(JSON.stringify({
   continue: true,
-  systemMessage: message
+  systemMessage: randomPrayer
 }));
